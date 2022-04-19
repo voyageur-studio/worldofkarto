e55bcd057c55a994436fce8bccec5b947bb90f0b
vaznok <jack@voyageur.studio>
2022-04-18 21:32:20 -0500

    a0.06
    Fix model paths

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -25,61 +25,61 @@ export default [
     {
         name: 'foxModel',
         type: 'gltfModel',
-        path: 'models/Fox/glTF-Binary/Fox.glb'
+        path: '../models/Fox/glTF-Binary/Fox.glb'
     },
     {
         name: 'landModel',
         type: 'gltfModel',
-        path: 'models/Land/Land.glb'
+        path: '../models/Land/Land.glb'
     },
     {
         name: 'darkspeareModel',
         type: 'gltfModel',
-        path: 'models/Cities/Darkspeare.glb'
+        path: '../models/Cities/Darkspeare.glb'
     },
     {
         name: 'lighthouselightModel',
         type: 'gltfModel',
-        path: 'models/Other/Lighthouse_light.glb'
+        path: '../models/Other/Lighthouse_light.glb'
     },
     {
         name: 'lighthouseModel',
         type: 'gltfModel',
-        path: 'models/Other/Lighthouse.glb'
+        path: '../models/Other/Lighthouse.glb'
     },
     {
         name: 'crossroadsModel',
         type: 'gltfModel',
-        path: 'models/Cities/Crossroads.glb'
+        path: '../models/Cities/Crossroads.glb'
     },
     {
         name: 'midfairModel',
         type: 'gltfModel',
-        path: 'models/Cities/Midfair.glb'
+        path: '../models/Cities/Midfair.glb'
     },
     {
         name: 'windmillModel',
         type: 'gltfModel',
-        path: 'models/Cities/Windmill.glb'
+        path: '../models/Cities/Windmill.glb'
     },
     {
         name: 'treesModel',
         type: 'gltfModel',
-        path: 'models/Land/Trees.glb'
+        path: '../models/Land/Trees.glb'
     },
     {
         name: 'gargadokModel',
         type: 'gltfModel',
-        path: 'models/Cities/Farms/Gargadok.glb'
+        path: '../models/Cities/Farms/Gargadok.glb'
     },
     {
         name: 'beerbogModel',
         type: 'gltfModel',
-        path: 'models/Cities/Farms/Beerbog.glb'
+        path: '../models/Cities/Farms/Beerbog.glb'
     },
     {
         name: 'moonvaleModel',
         type: 'gltfModel',
-        path: 'models/Cities/Moonvale.glb'
+        path: '../models/Cities/Moonvale.glb'
     },
 ]
